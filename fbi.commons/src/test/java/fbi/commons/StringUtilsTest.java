3a652c4ce47efc8eecef07e0f9d3220c7846abd2
Thasso Griebel <thasso.griebel@googlemail.com>
2011-05-16 14:32:51 +0000

    fixed test

--- fbi.commons/src/test/java/fbi/commons/StringUtilsTest.java
+++ fbi.commons/src/test/java/fbi/commons/StringUtilsTest.java
@@ -3,7 +3,6 @@ package fbi.commons;
 import org.junit.Test;
 
 import static junit.framework.Assert.assertEquals;
-import static junit.framework.Assert.fail;
 
 /**
  * @author Thasso Griebel (Thasso.Griebel@googlemail.com)
@@ -18,24 +17,10 @@ public class StringUtilsTest {
     @Test
     public void testComplement(){
         assertEquals("TGCAN-tgcanKkMmXxYy", StringUtils.complement("ACGTN-acgtnMmKkXxRr"));
-
-        try{
-            StringUtils.complement("B");
-            fail();
-        }catch (RuntimeException e){
-            // ignore
-        }
     }
     @Test
     public void testReverseComplement(){
         assertEquals("yYxXmMkKnacgt-NACGT", StringUtils.reverseComplement("ACGTN-acgtnMmKkXxRr"));
-
-        try{
-            StringUtils.complement("B");
-            fail();
-        }catch (RuntimeException e){
-            // ignore
-        }
     }
 
     @Test
