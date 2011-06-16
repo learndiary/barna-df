301cae6db1bad8449fc62e0e73e4598327f84829
Thasso Griebel <thasso.griebel@googlemail.com>
2011-06-16 16:35:24 +0000

    windows starter

--- fbi.genome.simulator/src/main/bin/flux.bat
+++ fbi.genome.simulator/src/main/bin/flux.bat
@@ -1,2 +1,15 @@
 @echo off
-java  -Xmx1G -XX:-UseBiasedLocking -DwrapperDir="'%~dp0'" -jar "%~dp0..\lib\FluxSimulator.jar" %*
+setlocal ENABLEDELAYEDEXPANSION
+::CONFIGURATION
+IF "%FLUX_MEM%" == "" set FLUX_MEM="1G"
+
+:: test for a java installation in the path
+for %%X in (java.exe) do (set FOUND=%%~$PATH:X)
+if not defined FOUND (
+   echo No Java installation found, please install Java ^>= 1.6 and make sure it is in your PATH.
+   exit /B 1
+)
+
+set parent=%~dp0%..\lib
+FOR /R %parent% %%G IN (*.jar) DO set CLASSPATH=!CLASSPATH!;%%G
+java -Xmx%FLUX_MEM% -cp %CLASSPATH% fbi.commons.flux.Flux %*
