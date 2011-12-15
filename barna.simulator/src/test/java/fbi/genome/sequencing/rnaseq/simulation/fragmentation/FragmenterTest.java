c23a5a9ff863253a81b73cb849bf65cb694388a1
Thasso Griebel <thasso.griebel@googlemail.com>
2011-12-15 16:42:23 +0000

    IO-15 - EmpiricalDistribution should be created dynamically IO-17 - Added line parser interface

---
+++ barna.simulator/src/test/java/fbi/genome/sequencing/rnaseq/simulation/fragmentation/FragmenterTest.java
@@ -0,0 +1,29 @@
+package fbi.genome.sequencing.rnaseq.simulation.fragmentation;
+
+import fbi.genome.sequencing.rnaseq.simulation.distributions.EmpiricalDistribution;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.net.URL;
+
+import static junit.framework.Assert.assertNotNull;
+import static junit.framework.Assert.fail;
+
+public class FragmenterTest {
+
+
+    @Test
+    public void testThatTheExpAllSizeDistributionSumIsValid(){
+            // load the default
+            URL url = Fragmenter.class.getResource("/expAll.isizes");
+            try {
+                // get the attributes ?
+                EmpiricalDistribution dist = EmpiricalDistribution.create(url.openStream(), 209208, 1, 297, 100, EmpiricalDistribution.LINE_PARSER_SINGLE_VALUE);
+                assertNotNull(dist);
+            } catch (IOException e) {
+                e.printStackTrace();
+                fail();
+            }
+
+    }
+}
