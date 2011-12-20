366525ee401f35f2884dfb903525f7ca208f39d5
Thasso Griebel <thasso.griebel@googlemail.com>
2011-12-20 18:20:01 +0000

    BARNA-85 removed Constants

--- barna.astalavista/src/main/java/barna/astalavista/AttributeExtractor.java
+++ barna.astalavista/src/main/java/barna/astalavista/AttributeExtractor.java
@@ -3,6 +3,10 @@ package barna.astalavista;
 import org.apache.commons.cli.*;
 
 public class AttributeExtractor {
+    public static final byte VERBOSE_NORMAL = 1;
+    public static final byte VERBOSE_SHUTUP = 0;
+
+    public static byte verboseLevel= VERBOSE_NORMAL;
 
 	private static Options options;
 	private static Options getOptions() {
@@ -21,7 +25,7 @@ public class AttributeExtractor {
 		try {
 			line= parser.parse(getOptions(), args);
 		} catch (ParseException e) {
-			if (Constants.verboseLevel> Constants.VERBOSE_SHUTUP)
+			if (verboseLevel> VERBOSE_SHUTUP)
 				System.err.println("[UPS] "+e.getMessage());
 			System.exit(-1);
 		}
