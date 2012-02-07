39d989cd3637ec8903f401e7034730f7c71c67ca
thasso <thasso.griebel@gmail.com>
2012-02-07 14:41:55 +0100

    more gtf tests and better error message for the gtf sorter

--- barna/barna.io/src/test/java/barna/io/gtf/GTFwrapperTest.java
+++ barna/barna.io/src/test/java/barna/io/gtf/GTFwrapperTest.java
@@ -21,4 +21,55 @@ public class GTFwrapperTest {
         assertTrue(new GTFwrapper(sorted).isApplicable());
         sorted.delete();
     }
+    @Test
+    public void testThatNormaltabsForFirstFieldsAreApplied() throws Exception {
+        File gzippedGtf = new File(getClass().getResource("/valid-tab-space.gtf").getFile());
+        assertTrue(gzippedGtf.exists());
+        GTFwrapper wrapperFile = new GTFwrapper(gzippedGtf);
+        File sorted = wrapperFile.sort();
+        assertNotNull(sorted);
+        assertTrue(new GTFwrapper(sorted).isApplicable());
+        sorted.delete();
+    }
+    @Test
+    public void testThatOnlySpaceSeparatedFields() throws Exception {
+        File gzippedGtf = new File(getClass().getResource("/only-space.gtf").getFile());
+        assertTrue(gzippedGtf.exists());
+        GTFwrapper wrapperFile = new GTFwrapper(gzippedGtf);
+        File sorted = wrapperFile.sort();
+        assertNotNull(sorted);
+        assertTrue(new GTFwrapper(sorted).isApplicable());
+        sorted.delete();
+    }
+    @Test
+    public void testMixedFileWithTabsAndSpaces() throws Exception {
+        File gzippedGtf = new File(getClass().getResource("/mixed-space.gtf").getFile());
+        assertTrue(gzippedGtf.exists());
+        GTFwrapper wrapperFile = new GTFwrapper(gzippedGtf);
+        File sorted = wrapperFile.sort();
+        assertNotNull(sorted);
+        assertTrue(new GTFwrapper(sorted).isApplicable());
+        sorted.delete();
+    }
+    @Test
+    public void testWithTabsInTheGTFFieldsAtTheEnd() throws Exception {
+        File gzippedGtf = new File(getClass().getResource("/tab-fields.gtf").getFile());
+        assertTrue(gzippedGtf.exists());
+        GTFwrapper wrapperFile = new GTFwrapper(gzippedGtf);
+        File sorted = wrapperFile.sort();
+        assertNotNull(sorted);
+        assertTrue(new GTFwrapper(sorted).isApplicable());
+        sorted.delete();
+    }
+    @Test
+    public void testThatInvalidGTFFailsWithReport() throws Exception {
+        File gzippedGtf = new File(getClass().getResource("/invalid.gtf").getFile());
+        assertTrue(gzippedGtf.exists());
+        GTFwrapper wrapperFile = new GTFwrapper(gzippedGtf);
+        try{
+            File sorted = wrapperFile.sort();
+            fail();
+        }catch (Exception error){
+        }
+    }
 }
