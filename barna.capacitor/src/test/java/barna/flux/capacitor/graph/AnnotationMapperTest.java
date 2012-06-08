f74ca6c0af7efeee766a95390b20c284ed0921ca
Emilio Palumbo <emiliopalumbo@gmail.com>
2012-06-08 18:30:51 +0200

    Testing SJ for paired reads - not working

--- barna.capacitor/src/test/java/barna/flux/capacitor/graph/AnnotationMapperTest.java
+++ barna.capacitor/src/test/java/barna/flux/capacitor/graph/AnnotationMapperTest.java
@@ -2,18 +2,14 @@ package barna.flux.capacitor.graph;
 
 import barna.flux.capacitor.reconstruction.FluxCapacitorSettings;
 import barna.io.BufferedIterator;
-import barna.io.BufferedIteratorRAM;
 import barna.io.bed.BEDwrapper;
 import barna.io.gtf.GTFwrapper;
 import barna.io.rna.UniversalReadDescriptor;
 import barna.model.Gene;
-import barna.model.Transcript;
-import barna.model.bed.BEDMapping;
 import junit.framework.Assert;
 import junit.framework.TestCase;
 import org.junit.Test;
 
-import java.awt.datatransfer.StringSelection;
 import java.io.*;
 import java.util.*;
 import java.util.zip.ZipOutputStream;
@@ -34,12 +30,12 @@ public class AnnotationMapperTest extends TestCase {
     @Override
     public void setUp() throws Exception {
         super.setUp();
-        setSettings();
+        setUpSettings();
     }
 
-    private void setSettings() {
+    private void setUpSettings() {
         UniversalReadDescriptor descriptor= new UniversalReadDescriptor();
-        descriptor.init(UniversalReadDescriptor.getDescriptor(UniversalReadDescriptor.DESCRIPTORID_SIMULATOR));
+        descriptor.init(UniversalReadDescriptor.getDescriptor(UniversalReadDescriptor.DESCRIPTORID_SIMPLE));
         settings = new FluxCapacitorSettings();
         settings.set(FluxCapacitorSettings.ANNOTATION_FILE,
                 new File(gtfFile.getAbsolutePath()));
@@ -52,7 +48,7 @@ public class AnnotationMapperTest extends TestCase {
         settings.set(FluxCapacitorSettings.KEEP_SORTED_FILES,
                 false);
         settings.set(FluxCapacitorSettings.ANNOTATION_MAPPING,
-                FluxCapacitorSettings.AnnotationMapping.PAIRED);
+                FluxCapacitorSettings.AnnotationMapping.SINGLE);
         settings.set(FluxCapacitorSettings.STDOUT_FILE,
                 null);
         settings.set(FluxCapacitorSettings.STATS_FILE,
@@ -70,40 +66,76 @@ public class AnnotationMapperTest extends TestCase {
         return sjs;
     }
 
+    private String getAltStrand(String s) {
+        if (s.equals("S"))
+            return "A";
+        if (s.equals("A"))
+            return "S";
+        return "";
+    }
+
 
     private Map<String,Integer> getSJReads(Gene g, boolean paired) throws Exception{
         BufferedReader bedReader = new BufferedReader(new InputStreamReader(new FileInputStream(bedFile)));
+        /*BufferedReader gtfReader = new BufferedReader(new InputStreamReader(new FileInputStream(gtfFile)));
+        ArrayList<String[]> nodes = new ArrayList<String[]>();*/
         Map<String,Integer> reads = new TreeMap<String, Integer>();
-        HashMap<String,String[]> hash = new HashMap<String, String[]>();
-        HashMap<String,String[]> p2Hash = new HashMap<String, String[]>();
+        HashMap<String,ArrayList<String[]>> p1hash = new HashMap<String, ArrayList<String[]>>();
+        HashMap<String,ArrayList<String[]>> p2hash = new HashMap<String, ArrayList<String[]>>();
         int nr = 0;
+        /*String tx = null;
+        for ( String line; (line = gtfReader.readLine())!= null;) {
+            String[] gLine = line.split("\t");
+            if (tx==null)
+                    tx = gLine[8].split(";")[0].split("\\s")[1];
+            if (gLine[2].equals("exon")) {
+                if (tx.equals(gLine[8].split(";")[0].split("\\s")[1])) {
+                    if (nodes.size() != 0) {
+                        if (!nodes.contains(new String[]{nodes.get(nodes.size()-1)[1],gLine[3]}))
+                            nodes.add(new String[]{nodes.get(nodes.size()-1)[1],gLine[3]});
+                    }
+                    if (!nodes.contains(new String[]{gLine[3],gLine[4]}))
+                        nodes.add(new String[]{gLine[3],gLine[4]});
+                } else {
+                    tx = gLine[8].split(";")[0].split("\\s")[1];
+                    if (!nodes.contains(new String[]{gLine[3],gLine[4]}))
+                        nodes.add(new String[]{gLine[3],gLine[4]});
+                }
+            }
+        }*/
         for ( String line; (line = bedReader.readLine())!= null;) {
             String[] bLine = line.split("\t");
             int nBlocks = Integer.parseInt(bLine[9]);
             if (g.getGeneID().equals(bLine[3].split(":")[0]+":"+ bLine[3].split(":")[1]))
             {
                 if (paired) {
-                    String readId = bLine[3].substring(0,bLine[3].length()-4);
-
-                    if (!hash.containsKey(readId)) {
-                        String[] sj = nBlocks==2?getId(bLine,nBlocks):null;
-                        hash.put(readId,sj);
-                    } else {
-                        if (nBlocks<=2) {
-                            String[] ids = hash.get(readId);
-                            if (ids != null) {
-                                nr = ids.length+nBlocks-1;
+                    if (nBlocks<3) {
+                        String readId = bLine[3].substring(0,bLine[3].length()-4);
+                        if (bLine[3].endsWith("1")) {
+                            if (!p1hash.containsKey(readId)) {
+                                String sj = nBlocks==2?getId(bLine,nBlocks)[0]:null;
+                                ArrayList<String[]> list = new ArrayList<String[]>();
+                                list.add(new String[]{bLine[3].substring(bLine[3].length()-3,bLine[3].length()-2),sj});
+                                p1hash.put(readId, list);
                             } else {
-                                ids = getId(bLine,nBlocks);
-                                nr = ids.length;
+                                String sj = nBlocks==2?getId(bLine,nBlocks)[0]:null;
+                                ArrayList<String[]> list = p1hash.get(readId);
+                                list.add(new String[]{bLine[3].substring(bLine[3].length()-3,bLine[3].length()-2), sj});
+                                p1hash.put(readId, list);
                             }
-                            for (String id : ids) {
-                                if (reads.containsKey(id)) {
-                                    nr += reads.get(id);
-                                }
-                                reads.put(id,nr);
+                        }
+                        if (bLine[3].endsWith("2")) {
+                            if (!p2hash.containsKey(readId)) {
+                                String sj = nBlocks==2?getId(bLine,nBlocks)[0]:null;
+                                ArrayList<String[]> list = new ArrayList<String[]>();
+                                list.add(new String[]{bLine[3].substring(bLine[3].length()-3,bLine[3].length()-2),sj});
+                                p2hash.put(readId, list);
+                            } else {
+                                String sj = nBlocks==2?getId(bLine,nBlocks)[0]:null;
+                                ArrayList<String[]> list = p2hash.get(readId);
+                                list.add(new String[]{bLine[3].substring(bLine[3].length()-3,bLine[3].length()-2), sj});
+                                p2hash.put(readId, list);
                             }
-                            hash.remove(readId);
                         }
                     }
                 } else {
@@ -120,20 +152,29 @@ public class AnnotationMapperTest extends TestCase {
                 }
             }
         }
-        /*if (paired) {
-            for (String k : hash.keySet()) {
-                if (hash.get(k)!=null) {
-                    String[] ids = hash.get(k);
-                    for (String id : ids) {
-                        nr = 1;
-                        if (reads.containsKey(id)) {
-                            nr += reads.get(id);
+        if (paired) {
+            for (String id : p1hash.keySet()) {
+                if (p2hash.containsKey(id)) {
+                    ArrayList<String[]> p1 = p1hash.get(id);
+                    ArrayList<String[]> p2 = p2hash.get(id);
+                    for (String[] s1 : p1) {
+                        for (String[] s2 : p2) {
+                            if (s2[0].equals(getAltStrand(s1[0]))) {
+                                if ((s1[1]!=null || s2[1] != null)) {
+                                    String sj = s1[1]!=null?s1[1]:s2[1];
+                                    nr = (s1[1]!=null?1:0) + (s2[1]!=null?1:0);
+                                    if (reads.containsKey(sj)) {
+                                        nr += reads.get(sj);
+                                    }
+                                    reads.put(sj,nr);
+                                }
+                                break;
+                            }
                         }
-                        reads.put(id,nr);
                     }
                 }
             }
-        }*/
+        }
         return reads;
     }
 
