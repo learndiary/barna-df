8dbd8ccbed571873f80960e5cc338976a2d99f2a
Emilio Palumbo <emiliopalumbo@gmail.com>
2012-09-26 15:07:59 +0200

    test data path as a system property

--- barna.capacitor/src/improvementTest/groovy/barna/flux/capacitor/improvementtest/FluxCapacitorRunner.groovy
+++ barna.capacitor/src/improvementTest/groovy/barna/flux/capacitor/improvementtest/FluxCapacitorRunner.groovy
@@ -4,12 +4,9 @@ import barna.commons.system.OSChecker
 import barna.flux.capacitor.reconstruction.FluxCapacitorSettings
 import barna.io.rna.UniversalReadDescriptor
 import com.martiansoftware.jsap.RequiredParameterMissingException
-import groovy.json.JsonSlurper
 import org.kamranzafar.jtar.TarInputStream
 
-import java.security.MessageDigest
 import java.util.zip.GZIPInputStream
-import java.util.zip.ZipInputStream
 
 /**
  *
@@ -28,15 +25,6 @@ class FluxCapacitorRunner {
      * the test data directory
      */
     private static Map testData
-    /**
-     * The default URL used to download Capacitor test data
-     */
-    private static String artifactoryUrl;
-
-    /**
-     * The repository to use
-     */
-    private static String repository
 
     /**
      * The test data artifact
@@ -50,8 +38,6 @@ class FluxCapacitorRunner {
 
     static {
         // initialize defaults
-        artifactoryUrl = System.getProperty("testdata.artifactory", "http://sammeth.net/artifactory")
-        repository = System.getProperty("testdata.repository", "repo")
         artifact = System.getProperty("testdata.artifact", "barna/test_data_imp-1.0.tgz")
         targetDirectory = System.getProperty("testdata.target", new File("").getAbsolutePath())
     }
@@ -127,10 +113,9 @@ class FluxCapacitorRunner {
      *
      * @param cwd working directory
      * @param parameterFile the parameter file
-     * @param denyTempDir if true, access to system tmp dir will be forbidden
      * @return output the output of the capacitor
      */
-    public static String runCapacitor(File cwd, File parameterFile, boolean denyTempDir = false){
+    public static String runCapacitor(File cwd, File parameterFile){
         synchronized (lock){
             if(executable == null){
                 executable = System.getProperty("dist.exe")
@@ -157,10 +142,6 @@ class FluxCapacitorRunner {
 
         def pb = new ProcessBuilder()
         pb.environment().put("FLUX_MEM", "1G")
-
-        if (denyTempDir){
-            pb.environment().put("JAVA_OPTS", "-Dflux.io.deny.tmpdir=yes")
-        }
 
         def cmd = [executable, "-p", parameterFile.getAbsolutePath()]
         if (OSChecker.isWindows()) {
@@ -192,7 +173,7 @@ class FluxCapacitorRunner {
     static synchronized Map getTestData() {
         if (this.testData == null) {
             this.testData = [:]
-            File testDir = prepareTestData()
+            File testDir = new File(System.getProperty("test.data"))
             testDir.eachFileRecurse {file ->
                 def relPath = file.getAbsolutePath().replace(testDir.getAbsolutePath(), "")
                 relPath = relPath.replaceAll("\\\\", "/")
@@ -202,115 +183,6 @@ class FluxCapacitorRunner {
         }
         return this.testData
     }
-
-    /**
-     * Checks for valid local test data or downloads test data from the repository
-     *
-     * @return test_data the unzipped test_data directory
-     */
-    private static File prepareTestData() {
-        println("Checking for test data")
-        def slurper = new JsonSlurper()
-        //def dataFQN = new URL("${artifactoryUrl}/api/storage/${repository}/${artifact}")
-        //def fileName = dataFQN.getFile().split("/").last()
-        def fileName = "test_data_imp-1.0.tgz"
-
-        def targetFile = new File(targetDirectory, fileName)
-        File test_data_dir = new File(targetDirectory, "test_data_imp")
-        def md5File = new File(targetDirectory, "${fileName}.md5")
-
-        //def metaData = slurper.parseText(dataFQN.openStream().text)
-        //def md5sum = metaData.checksums['md5']
-
-        boolean invalid_file = false
-        if (!targetFile.exists()) {//|| !md5File.exists() || md5File.readLines()[0].trim() != md5sum) {
-            invalid_file = true
-        }
-
-        /*if (invalid_file) {
-            println("Downloading test data from ${dataFQN.toExternalForm()}")
-            if (targetFile.exists()) {
-                targetFile.delete()
-            }
-            if (test_data_dir.exists()) {
-                FileHelper.rmDir(new File(targetDirectory, "test_data_imp"))
-            }
-            if (md5File.exists()) {
-                md5File.delete()
-            }
-            // download
-            def out = new BufferedOutputStream(new FileOutputStream(targetFile))
-            out << new URL(metaData['downloadUri']).openStream()
-            out.close()
-
-            // compare md5
-            if (md5sum != generateMD5(targetFile)) {
-                throw new RuntimeException("Test Data downlaoded but md5 sums do not match !")
-            }
-
-            // write md5 file
-            md5File.write(md5sum)
-        }*/
-
-
-        if (!test_data_dir.exists()) {
-            println "Unzipping test data"
-            untar(targetFile, targetDirectory)
-        }
-        println("Test data available")
-        return test_data_dir
-    }
-
-    /**
-     * Helper method to generate the md5 sum for a given file
-     *
-     * @param file the file
-     * @return md5sum the hex representation of the md5sum
-     */
-    private static String generateMD5(final file) {
-        MessageDigest digest = MessageDigest.getInstance("MD5")
-        file.withInputStream() {is ->
-            byte[] buffer = new byte[8192]
-            int read = 0
-            while ((read = is.read(buffer)) > 0) {
-                digest.update(buffer, 0, read);
-            }
-        }
-        byte[] md5sum = digest.digest()
-        BigInteger bigInt = new BigInteger(1, md5sum)
-        return bigInt.toString(16).padLeft(32, '0')
-    }
-
-    /**
-     * Helper to unzip a file
-     */
-    private static unzip = { File file, String dest ->
-        def result = new ZipInputStream(new FileInputStream(file))
-        def destFile = new File(dest)
-        if (!destFile.exists()) {
-            destFile.mkdir();
-        }
-        result.withStream {
-            def entry
-            while (entry = result.nextEntry) {
-                if (!entry.isDirectory()) {
-                    new File(dest + File.separator + entry.name).parentFile?.mkdirs()
-                    def output = new FileOutputStream(dest + File.separator
-                            + entry.name)
-                    output.withStream {
-                        int len = 0;
-                        byte[] buffer = new byte[4096]
-                        while ((len = result.read(buffer)) > 0) {
-                            output.write(buffer, 0, len);
-                        }
-                    }
-                } else {
-                    new File(dest + File.separator + entry.name).mkdir()
-                }
-            }
-        }
-    }
-
 
     /**
      * Helper to untar a file
