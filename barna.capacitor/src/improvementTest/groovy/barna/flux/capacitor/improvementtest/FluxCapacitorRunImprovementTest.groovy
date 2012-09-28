627b1d5999eea874ff4a583ed2cf77d4aa93863a
Emilio Palumbo <emiliopalumbo@gmail.com>
2012-09-28 10:04:35 +0200

    test with full dataset

--- barna.capacitor/src/improvementTest/groovy/barna/flux/capacitor/improvementtest/FluxCapacitorRunImprovementTest.groovy
+++ barna.capacitor/src/improvementTest/groovy/barna/flux/capacitor/improvementtest/FluxCapacitorRunImprovementTest.groovy
@@ -53,9 +53,9 @@ class FluxCapacitorRunImprovementTest {
 
     @After
     public void cleanup(){
-        if(currentTestDirectory != null){
+        /*if(currentTestDirectory != null){
             FileHelper.rmDir(currentTestDirectory)
-        }
+        }*/
     }
 
     @Test
