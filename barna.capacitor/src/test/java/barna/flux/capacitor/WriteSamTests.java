bd52a2fd9d24b656309f063a8c93e7ba89625d85
Thasso Griebel <thasso.griebel@gmail.com>
2012-12-12 12:59:38 +0100

    Added test for bed file

--- barna.capacitor/src/test/java/barna/flux/capacitor/WriteSamTests.java
+++ barna.capacitor/src/test/java/barna/flux/capacitor/WriteSamTests.java
@@ -17,7 +17,8 @@ public class WriteSamTests {
         FluxCapacitorSettings settings = new FluxCapacitorSettings();
         //settings.set(FluxCapacitorSettings.ANNOTATION_FILE, new File("/Users/thasso/data/capacitor-test-data/chr21.gtf"));
         settings.set(FluxCapacitorSettings.ANNOTATION_FILE, new File(getClass().getResource("/single_multimap.gtf").getFile()));
-        settings.set(FluxCapacitorSettings.MAPPING_FILE, new File(getClass().getResource("/single_multimap.bam").getFile()));
+        //settings.set(FluxCapacitorSettings.MAPPING_FILE, new File(getClass().getResource("/single_multimap.bam").getFile()));
+        settings.set(FluxCapacitorSettings.MAPPING_FILE, new File(getClass().getResource("/single_multimap.bed").getFile()));
         settings.set(FluxCapacitorSettings.SORT_IN_RAM, true);
         FluxCapacitor capacitor = new FluxCapacitor(settings);
         capacitor.call();
