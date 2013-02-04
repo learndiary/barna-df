3f6505881f2ded1d939b5e0a79035a0889adfb82
Emilio Palumbo <emiliopalumbo@gmail.com>
2013-02-04 14:28:08 +0100

    Added test for output gtf

--- barna.capacitor/src/test/java/barna/flux/capacitor/FluxCapacitorTest.java
+++ barna.capacitor/src/test/java/barna/flux/capacitor/FluxCapacitorTest.java
@@ -276,7 +276,7 @@ public class FluxCapacitorTest {
         assertEquals(loaded.getEventsExp(), stats.getEventsExp());
         assertEquals(loaded.getReadsSingleTxLoci(), stats.getReadsSingleTxLoci());
         assertEquals(loaded.getMappingsSingleTxLoci(), stats.getMappingsSingleTxLoci());
-        assertEquals(loaded.getMappingPairs(), stats.getMappingPairs());
+        assertEquals(loaded.getMappingPairsSingleTxLoci(), stats.getMappingPairsSingleTxLoci());
         assertEquals(loaded.getMappingsTotal(), stats.getMappingsTotal());
         assertEquals(loaded.getMappingsMapped(), stats.getMappingsMapped());
         assertEquals(loaded.getMappingPairsNoTx(), stats.getMappingPairsNoTx());
@@ -593,13 +593,13 @@ public class FluxCapacitorTest {
         Map pars = new HashMap();
         pars.put("ANNOTATION_FILE", GTF_MM9_SORTED);
         pars.put("MAPPING_FILE", BED_MM9_SORTED);
-        pars.put("PROFILE_FILE", BED_MM9_PROFILE);
+        //pars.put("PROFILE_FILE", BED_MM9_PROFILE);
         pars.put("READ_DESCRIPTOR", UniversalReadDescriptor.DESCRIPTORID_SIMULATOR);
         pars.put("ANNOTATION_MAPPING", AnnotationMapping.PAIRED);
 
         File parFile = FluxCapacitorRunner.createTestDir(currentTestDirectory, pars);
         String[] params = {"--profile", "-p", parFile.getAbsolutePath()};
-        FluxCapacitorRunner.runCapacitor(parFile, params);
+        //FluxCapacitorRunner.runCapacitor(parFile, params);
         FluxCapacitorRunner.runCapacitor(parFile,null);
 
         // check
@@ -630,49 +630,5 @@ public class FluxCapacitorTest {
         for (int i = 0; i < refLines.size(); i++) {
             assertEquals(runLines.get(i), refLines.get(i));
         }
-    }
-
-    @Test
-    public void testGtex() throws Exception {
-        File proFile = new File(currentTestDirectory, FileHelper.append(FluxCapacitorRunner.DEFAULT_OUTPUT_FILE.toString(), ".profiles", true, ""));
-
-        Map pars = new HashMap();
-        pars.put("ANNOTATION_FILE", "/users/rg/epalumbo/annotations/hg/gencode.v12.annotation.patched_contigs_sorted.gtf");
-        pars.put("MAPPING_FILE", "/users/rg/projects/GTEx/bam/june/GTEX-QV31-0005-SM-2S1Q6.bam");
-        pars.put("COUNT_ELEMENTS", "[SPLICE_JUNCTIONS,INTRONS]");
-        pars.put("PROFILE_FILE", proFile);
-        //pars.put("READ_DESCRIPTOR", UniversalReadDescriptor.DESCRIPTORID_SIMPLE);
-        //pars.put("ANNOTATION_MAPPING", AnnotationMapping.AUTO);
-
-        File parFile = FluxCapacitorRunner.createTestDir(currentTestDirectory, pars);
-        String[] params = {"--profile", "-p", parFile.getAbsolutePath()};
-        FluxCapacitorRunner.runCapacitor(parFile, params);
-        FluxCapacitorRunner.runCapacitor(parFile,null);
-
-        File output = new File(currentTestDirectory, FluxCapacitorRunner.DEFAULT_OUTPUT_FILE.toString());
-        File refOutput = new File("/data/epalumbo/gtex/fluxrelease/GTEX-QV31-0005-SM-2S1Q6.gtf");
-
-        BufferedReader runGtf = new BufferedReader(new FileReader(refOutput));
-        BufferedReader refGtf = new BufferedReader(new FileReader(output));
-
-        List<String> runLines = new ArrayList<String>();
-        List<String> refLines = new ArrayList<String>();
-
-        String line;
-        while ((line = runGtf.readLine()) != null) {
-            runLines.add(line);
-        }
-        runGtf.close();
-        while ((line = refGtf.readLine()) != null) {
-            refLines.add(line);
-        }
-        refGtf.close();
-
-        assertEquals(refLines.size(),runLines.size());
-
-        for (int i = 0; i < refLines.size(); i++) {
-            assertEquals(runLines.get(i), refLines.get(i));
-        }
-
     }
 }
