2756012bdd1226182b97c69765d1936ed216f5d2
micha <micha@sammeth.net>
2013-04-29 19:07:37 -0300

    Merge remote branches 'origin/astalavista' and 'origin/develop' into astalavista

---
+++ barna.tools/src/main/java/barna/genome/tools/overlap/BEDdistance.java
@@ -0,0 +1,11 @@
+package barna.genome.tools.overlap;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: micha
+ * Date: 2/7/13
+ * Time: 1:20 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class BEDdistance {
+}
